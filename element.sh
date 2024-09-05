086ed036c5657890edbc297039c4904e52c48dca
Pavel Lutskovsky <lutskovp@gmail.com>
2024-09-05 07:13:32 +0000

    feat: database updates
    changes periodic_table.sql mode to be executable
    
    updates elements.symbol leading letter to upper case
    
    update trailing zeros
    
    feat: adds fluorine and neon. completes db fix
    
    feat: adds database access
    
    minor refactor
    
    refactor output
    
    minor element.sh refactor
    
    fix element.sh output issues
    
    chore: adding some comments

--- element.sh
+++ element.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
-echo Please provide an element as an argument.
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+ATOMIC_NUMBER=1
+SYMBOL=H
+NAME=Hydrogen
+EXPECTED_OUTPUT="The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+
+if [[ -z $1 ]]
+then 
+  echo Please provide an element as an argument.
+else 
+
+  if [[ $1 =~ ^[0-9]+$ ]] # if the argument is a number, treat it as an atomic_number int
+  then
+    ATOMIC_NUMBER=$1
+    ELEMENT_RESULT=$($PSQL "SELECT elements.atomic_number, elements.symbol, elements.name, properties.type, properties.atomic_mass, properties.boiling_point_celsius, properties.melting_point_celsius FROM elements inner join properties on elements.atomic_number = properties.atomic_number WHERE elements.atomic_number = $ATOMIC_NUMBER")
+  else # otherwise, it could be a symbol or a name. symbols are up to 3 characters. names are all longer, but just matching on either for now
+    SEARCH_TERM="$1"
+    ELEMENT_RESULT=$($PSQL "SELECT elements.atomic_number, elements.symbol, elements.name, properties.type, properties.atomic_mass, properties.boiling_point_celsius, properties.melting_point_celsius FROM elements inner join properties on elements.atomic_number = properties.atomic_number WHERE elements.name = '$SEARCH_TERM' OR elements.symbol = '$SEARCH_TERM'")
+  fi
+
+  if [[ -z $ELEMENT_RESULT ]]
+  then
+    echo I could not find that element in the database.
+  else
+    IFS='|' ELEMENT=($ELEMENT_RESULT)
+
+    # echo ${ELEMENT[0]} atomic_number
+    # echo ${ELEMENT[1]} symbol
+    # echo ${ELEMENT[2]} name
+    # echo ${ELEMENT[3]} type
+    # echo ${ELEMENT[4]} atomic_mass
+    # echo ${ELEMENT[5]} boiling_point
+    # echo ${ELEMENT[6]} melting_point
+
+    echo "The element with atomic number ${ELEMENT[0]} is ${ELEMENT[2]} (${ELEMENT[1]}). It's a ${ELEMENT[3]}, with a mass of ${ELEMENT[4]} amu. ${ELEMENT[2]} has a melting point of ${ELEMENT[6]} celsius and a boiling point of ${ELEMENT[5]} celsius."
+  fi  
+fi
+
