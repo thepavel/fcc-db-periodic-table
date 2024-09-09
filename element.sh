a86bdee5106c2eef4f7b26324a7e921135c5cd71
Pavel Lutskovsky <lutskovp@gmail.com>
2024-09-09 04:39:46 +0000

    chore: adding some comments

--- element.sh
+++ element.sh
@@ -11,25 +11,31 @@ if [[ -z $1 ]]
 then 
   echo Please provide an element as an argument.
 else 
-  if [[ $1 =~ ^[0-9]+$ ]] 
+
+  if [[ $1 =~ ^[0-9]+$ ]] # if the argument is a number, treat it as an atomic_number int
   then
     ATOMIC_NUMBER=$1
     ELEMENT_RESULT=$($PSQL "SELECT elements.atomic_number, elements.symbol, elements.name, properties.type, properties.atomic_mass, properties.boiling_point_celsius, properties.melting_point_celsius FROM elements inner join properties on elements.atomic_number = properties.atomic_number WHERE elements.atomic_number = $ATOMIC_NUMBER")
-  else 
+  else # otherwise, it could be a symbol or a name. symbols are up to 3 characters. names are all longer, but just matching on either for now
     SEARCH_TERM="$1"
     ELEMENT_RESULT=$($PSQL "SELECT elements.atomic_number, elements.symbol, elements.name, properties.type, properties.atomic_mass, properties.boiling_point_celsius, properties.melting_point_celsius FROM elements inner join properties on elements.atomic_number = properties.atomic_number WHERE elements.name = '$SEARCH_TERM' OR elements.symbol = '$SEARCH_TERM'")
   fi
 
-  IFS='|' ELEMENT=($ELEMENT_RESULT)
+  if [[ -z $ELEMENT_RESULT ]]
+  then
+    echo I could not find that element in the database.
+  else
+    IFS='|' ELEMENT=($ELEMENT_RESULT)
 
-  # echo ${ELEMENT[0]} atomic_number
-  # echo ${ELEMENT[1]} symbol
-  # echo ${ELEMENT[2]} name
-  # echo ${ELEMENT[3]} type
-  # echo ${ELEMENT[4]} atomic_mass
-  # echo ${ELEMENT[5]} boiling_point
-  # echo ${ELEMENT[6]} melting_point
+    # echo ${ELEMENT[0]} atomic_number
+    # echo ${ELEMENT[1]} symbol
+    # echo ${ELEMENT[2]} name
+    # echo ${ELEMENT[3]} type
+    # echo ${ELEMENT[4]} atomic_mass
+    # echo ${ELEMENT[5]} boiling_point
+    # echo ${ELEMENT[6]} melting_point
 
-  echo "The element with atomic number ${ELEMENT[0]} is ${ELEMENT[2]} (${ELEMENT[1]}). It's a ${ELEMENT[3]}, with a mass of ${ELEMENT[4]} amu. ${ELEMENT[2]} has a melting point of ${ELEMENT[6]} celsius and a boiling point of ${ELEMENT[5]} celsius."
+    echo "The element with atomic number ${ELEMENT[0]} is ${ELEMENT[2]} (${ELEMENT[1]}). It's a ${ELEMENT[3]}, with a mass of ${ELEMENT[4]} amu. ${ELEMENT[2]} has a melting point of ${ELEMENT[6]} celsius and a boiling point of ${ELEMENT[5]} celsius."
+  fi  
 fi
 
